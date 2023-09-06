3f60e2045101acd050215984f4987ba20f54800a
Kasim Saifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-09-06 19:05:22 +0530

    done

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
 const express = require('express');
-const { execFile } = require('child_process');
-const fs = require('fs');
-const cors = require('cors');
+const { exec } = require('child_process');
+const cors = require('cors'); // Import the cors middleware
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Enable CORS for all routes
+// Enable CORS for all route
 app.use(cors());
 
 // Endpoint to execute Node.js code
 app.post('/execute', (req, res) => {
   const { code } = req.body;
 
-  // Create a temporary JavaScript file
-  const tempFileName = `temp_${Date.now()}.js`;
-  fs.writeFileSync(tempFileName, code);
-
-  // Execute the temporary file
-  execFile('node', [tempFileName], (error, stdout, stderr) => {
-    // Delete the temporary file
-    fs.unlinkSync(tempFileName);
-
+  exec(`node -e "${code}"`, (error, stdout, stderr) => {
     if (error) {
       console.error('Execution Error:', error);
-      return res.status(500).json({ error: 'An error occurred while executing the code.', stderr });
+      return res.status(500).json({ error: 'An error occurred while executing the code.' });
     }
 
     if (stderr) {
       console.error('Script Error:', stderr);
-      return res.status(400).json({ error: stderr });
+      return res.status(400).json({ error: 'There was an error in your code.' });
     }
 
     console.log('Execution Result:', stdout);
